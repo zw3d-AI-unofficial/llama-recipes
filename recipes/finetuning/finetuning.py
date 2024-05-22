e05e12ad537f50f723fe2fa15c1466bab64a171f
XuHaizi <xuhaizi@zwcad.com>
2024-05-22 10:16:43 +0800

    fix working dictionary bug

--- recipes/finetuning/finetuning.py
+++ recipes/finetuning/finetuning.py
@@ -2,7 +2,7 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 import fire
-from llama_recipes.finetuning import main
+from src.llama_recipes.finetuning import main
 
 if __name__ == "__main__":
     fire.Fire(main)
